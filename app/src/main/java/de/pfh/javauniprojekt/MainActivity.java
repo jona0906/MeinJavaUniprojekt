31c5b45ab203e66d21777ca577be36082b8edf6b
jona0906 <130446651+jona0906@users.noreply.github.com>
2023-08-29 19:35:14 +0200

    Initial commit

---
+++ app/src/main/java/de/pfh/javauniprojekt/MainActivity.java
@@ -0,0 +1,41 @@
+package de.pfh.javauniprojekt;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.FirebaseApp;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button register;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseApp.initializeApp(this);
+
+        register = findViewById(R.id.register); // Verknüpfen der erstellten Variable und der Buttons
+        login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+                finish();
+            }
+        });
+    }
+}
